dff6dd8466771384fe1b7f7953dd79abcd4f9bd6
SebastienLaurentt <slaurent.26@gmail.com>
2024-01-25 23:22:10 +0100

    nesting ThemeProvider in body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@/src/theme/ThemeProvider";
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr" className="h-full">
+      <body className={clsx(inter.className, "bg-background h-full")}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
