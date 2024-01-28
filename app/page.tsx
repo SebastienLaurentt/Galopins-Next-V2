c463da467aa723d00568003d37e2b9dd82491a62
SebastienLaurentt <slaurent.26@gmail.com>
2024-01-29 00:06:03 +0100

    add some content in home and create tailwind breakpoints

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
+import img from "../public/photos.webp"
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <main className="py-4">
+      <h1> Les Galopins </h1>
+      <h2> Club de randonnée à Montélimar </h2>
+      <Image src={img} alt="Les Galopins" width={500} height={500} className="w-full" />
+    </main>
   );
 }
