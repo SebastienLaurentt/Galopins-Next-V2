cfd82ae350d0ac7ae964c7f40b0f92fe46b2a220
SebastienLaurentt <slaurent.26@gmail.com>
2024-01-28 23:21:16 +0100

    create 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
+export default function NotFound() {
+
+  return (
+    <div className="text-center lg:py-4">
+      <h1 className="text-3xl">
+        Page non existante
+      </h1>
+      <Button>
+        <Link href="/"> Retourner à l'accueil </Link>
+      </Button>
+    </div>
+  );
+}
