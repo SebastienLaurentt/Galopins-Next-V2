ac2ce2cd5ae20898159b2194df86d1a7e138ac4d
SebastienLaurentt <slaurent.26@gmail.com>
2024-01-30 00:09:05 +0100

    use next img as bg and with header and content inside with z index

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
 import Image from "next/image";
 
-import img from "../public/photos.webp"
+import img from "../public/home.webp";
 
 export default function Home() {
   return (
-    <main className="py-4">
-      <h1> Les Galopins </h1>
-      <h2> Club de randonnée à Montélimar </h2>
-      <Image src={img} alt="Les Galopins" width={500} height={500} className="w-full" />
-    </main>
+    // <main className="">
+    //   <div className="">
+    //     <Image
+    //       src={img}
+    //       alt="Les Galopins"
+    //       className="w-full -z-10"
+    //     />
+    //     <div className="absolute">
+
+    //     </div>
+
+    //   </div>
+    // </main>
+    <div className="relative bg-black bg-opacity-50 ">
+      <div className="py-72 text-white">
+        <h1> Les Galopins </h1>
+        <h2> Club de randonnée à Montélimar </h2>
+      </div>
+      <Image
+        src={img}
+        alt="Les Galopins"
+        fill={true}
+        className="object-cover -z-10"
+      />
+    </div>
   );
 }
--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-slate-950 text-white">
+    <header className="absolute text-white w-screen z-10 ">
       <div className="flex items-center px-6 py-4 justify-between">
         <span className="font-bold"> Les Galopins </span>
         <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
