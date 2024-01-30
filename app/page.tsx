8391bf69d62b7709a94210fb538da03841d3a6fb
SebastienLaurentt <slaurent.26@gmail.com>
2024-01-30 20:43:01 +0100

    update header and home css

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,15 @@ import img from "../public/home.webp";
 export default function Home() {
   return (
     <main>
-      <div className="relative bg-black bg-opacity-20 bg-top">
+      <div className="relative bg-black bg-opacity-60 bg-top">
         <div className="min-h-[calc(100vh/1.3)] px-6 text-white ">
-          <div className="pt-40">
-            <h1> Les Galopins </h1>
-            <h2> Club de randonnée à Montélimar </h2>
+          <div className="pt-40 text-center">
+            <h1 className="mb-2 leading-10"> LES GALOPINS </h1>
+            <p className="font-medium text-lg w-4/5 mx-auto">
+              {" "}
+              Club de randonnée pédestre à Montélimar{" "}
+            </p>
           </div>
-
         </div>
         <Image
           src={img}
--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
+import logo from "../../../../public/Logo-Black.svg";
 import BurgerMenu from "./BurgerMenu";
 
 export const Header = () => {
@@ -8,8 +10,8 @@ export const Header = () => {
 
   return (
     <header className="absolute text-white w-screen z-10 ">
-      <div className="flex items-center px-6 py-4 justify-between">
-        <span className="font-bold"> Les Galopins </span>
+      <div className="flex items-center px-6 py-4 justify-between ">
+        <Image src={logo} alt="Logo Galopins" className="w-16" />
         <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
     </header>
