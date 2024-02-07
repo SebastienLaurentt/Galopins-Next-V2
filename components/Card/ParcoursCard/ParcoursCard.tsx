bc787c17c5873aa8867945869038668a2550b9fb
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-07 17:53:47 +0100

    update border in parcours card

--- components/Card/ParcoursCard/ParcoursCard.tsx
+++ components/Card/ParcoursCard/ParcoursCard.tsx
@@ -10,7 +10,7 @@ interface ParcoursCardProps {
 
 const ParcoursCard = ({parcoursTitle, area, frequency, case1, case2 }: ParcoursCardProps) => {
   return (
-    <div className='flex flex-col max-w-[360px] md:max-w-[480px] mx-auto  mb-10 text-center border border-2 border-green-800 rounded '>
+    <div className='flex flex-col max-w-[360px] md:max-w-[480px] mx-auto  mb-10 text-center  rounded '>
       <div className='bg-green-800 py-4'>
         <h3 className='text-white'>
           {parcoursTitle}
@@ -19,19 +19,19 @@ const ParcoursCard = ({parcoursTitle, area, frequency, case1, case2 }: ParcoursC
           {area}
         </span>
       </div>
-        <div className='py-4 md:text-md'>
-          <span>
-            {frequency}
-          </span>
-          <ul>
-            <li>
-              {case1}
-            </li>
-            <li>
-              {case2}
-            </li>
-          </ul>
-        </div>
+      <div className='py-4 md:text-md  border-b-2 border-r-2 border-l-2  border-green-800'>
+        <span>
+          {frequency}
+        </span>
+        <ul>
+          <li>
+            {case1}
+          </li>
+          <li>
+            {case2}
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
