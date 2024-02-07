3e1db0fedce73598294f612c6935d915d8f8d2fa
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-07 17:41:20 +0100

    responsive md for main mb and SeeMoreButton mt

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,9 @@ export default function Home() {
         minHeightScreen={false}
         className="flex flex-col justify-center"
       >
-        <div className="pb-4 text-center max-w-[280px] mx-auto text-white">
-          <h1 className="mb-2"> LES GALOPINS </h1>
-          <p className="font-medium text-md w-4/5 mx-auto mb-2 leading-6">
+        <div className="pb-4 md:pb-6 text-center max-w-[280px] md:max-w-[400px] mx-auto text-white">
+          <h1 className="mb-2 md:mb-4"> LES GALOPINS </h1>
+          <p className="font-medium text-md md:text-xl w-4/5 mx-auto mb-2 md:mb-4 leading-6 md:leading-8">
             Club de randonnée pédestre à Montélimar
           </p>
           <Button asChild variant="hero">
