19f95a23124db1e9478b2c71c9fbad11e5a20f3e
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-07 17:29:02 +0100

    responsive md for ParcoursCard

--- components/Card/ParcoursCard/ParcoursCard.tsx
+++ components/Card/ParcoursCard/ParcoursCard.tsx
@@ -10,16 +10,16 @@ interface ParcoursCardProps {
 
 const ParcoursCard = ({parcoursTitle, area, frequency, case1, case2 }: ParcoursCardProps) => {
   return (
-    <div className='flex flex-col mb-10 text-center border border-2 border-green-800 rounded '>
+    <div className='flex flex-col max-w-[360px] md:max-w-[480px] mx-auto  mb-10 text-center border border-2 border-green-800 rounded '>
       <div className='bg-green-800 py-4'>
         <h3 className='text-white'>
           {parcoursTitle}
         </h3>
-        <span className='mb-2 text-white'>
+        <span className='mb-2 text-white md:text-md'>
           {area}
         </span>
       </div>
-        <div className='py-4'>
+        <div className='py-4 md:text-md'>
           <span>
             {frequency}
           </span>
