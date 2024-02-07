92799bf71ffca44559169c636428a6eee49c2b3f
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-07 16:08:26 +0100

    add some css in ParcoursCard

--- components/Card/ParcoursCard/ParcoursCard.tsx
+++ components/Card/ParcoursCard/ParcoursCard.tsx
@@ -10,24 +10,28 @@ interface ParcoursCardProps {
 
 const ParcoursCard = ({parcoursTitle, area, frequency, case1, case2 }: ParcoursCardProps) => {
   return (
-    <div className='flex flex-col mb-10 text-center bg-green-800 text-white py-4 rounded-lg'>
-      <h3 className='text-white'>
-        {parcoursTitle}
-      </h3>
-      <span className='mb-2'>
-        {area}
-      </span>
-      <span>
-        {frequency}
-      </span>
-      <ul>
-        <li>
-          {case1}
-        </li>
-        <li>
-          {case2}
-        </li>
-      </ul>
+    <div className='flex flex-col mb-10 text-center border border-2 border-green-800 rounded '>
+      <div className='bg-green-800 py-4'>
+        <h3 className='text-white'>
+          {parcoursTitle}
+        </h3>
+        <span className='mb-2 text-white'>
+          {area}
+        </span>
+      </div>
+        <div className='py-4'>
+          <span>
+            {frequency}
+          </span>
+          <ul>
+            <li>
+              {case1}
+            </li>
+            <li>
+              {case2}
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
