6edd586b92bec2a0f3cddb9f72a3eb7ec66d4d3d
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-08 00:48:14 +0100

    responsive md for burgerMenu

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <footer className="bg-slate-950 text-white">
-      <div className="flex items-center px-6 py-4 justify-between">
-        <span className="text-xs">@2024 - Les Galopins</span>
-        <span className="text-xs">
+      <div className="flex text-xs md:text-sm items-center px-6 md:px-10 py-4 justify-between ">
+        <span className="">@2024 - Les Galopins</span>
+        <span className="">
           <Link href="/mentions"> Mentions légales </Link>
         </span>
       </div>
