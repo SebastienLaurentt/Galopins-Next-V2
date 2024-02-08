ea07fbab24e68937660b7cd0f0c9d70a3ce7da35
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-08 11:23:33 +0100

    increases footer py and ScrollToTopButton is now on bottom-14 whereas bottom-4

--- components/Button/ScrollToTopButton/ScrollToTopButton.tsx
+++ components/Button/ScrollToTopButton/ScrollToTopButton.tsx
@@ -31,7 +31,7 @@ const ScrollToTopButton = () => {
 
   return (
     <button
-      className={`fixed bottom-4 right-6 md:right-10 rounded-xl p-2 outline-none transition-opacity duration-200  ${
+      className={`fixed bottom-14 right-6 md:right-10 rounded-xl p-1 outline-none transition-opacity duration-200 bg-white  ${
         isVisible ? "opacity-100" : "opacity-0"
       }`}
       onClick={scrollToTop}
