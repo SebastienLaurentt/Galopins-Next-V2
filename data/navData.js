d8fb5f04a764120104b4a2fdd34a3291fcf5f991
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-08 16:22:27 +0100

    create navData file

---
+++ data/navData.js
@@ -0,0 +1,29 @@
+const navData = [
+  {
+      href: '/',
+      name: 'Accueil',
+      burgerMenuName:'Accueil',
+  },
+  {
+      href: '/club',
+      name: 'Club',
+      burgerMenuName:'Le Club',
+  },
+  {
+      href: '/parcours',
+      name: 'Parcours',
+      burgerMenuName:'Les Parcours',
+  },
+  {
+      href: '/photos',
+      name: 'Photos',
+      burgerMenuName:'Nos Photos',
+  },
+  {
+      href: '/adhesion',
+      name: 'Adhésion',
+      burgerMenuName:'Adhésion',
+  },
+];
+
+export default navData;
