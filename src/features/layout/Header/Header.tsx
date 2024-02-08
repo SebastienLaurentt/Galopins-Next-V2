0d0f925f31028a2ccb29d675e7e32f95c41b7fd0
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-08 14:24:43 +0100

    update header to allow different position and bg in home in relation to other pages

--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -5,13 +5,18 @@ import Link from "next/link";
 import { useState } from "react";
 import logo from "../../../../public/Logo-Black.svg";
 import BurgerMenu from "./BurgerMenu";
+import { usePathname } from 'next/navigation'
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname()
+
+  const headerBgColor = pathname === "/" ? "" : "bg-slate-950";
+  const headerPosition = pathname === "/" ? "absolute" : "";
 
   return (
-    <header className="absolute text-white w-full z-10 ">
-      <div className="flex items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-4 justify-between   ">
+    <header className={`${headerPosition} text-white w-full z-10 ${headerBgColor}`}>
+      <div className="flex items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-4 justify-between">
         <Link href="/">
           <Image src={logo} alt="Logo Galopins" className="w-16 md:w-20 lg:w-24" />
         </Link>
