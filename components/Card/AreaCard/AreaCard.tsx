ee9b84c3c2b68d01856a61071d94ccd50de28e0d
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-08 20:18:43 +0100

    tags value are now props

--- components/Card/AreaCard/AreaCard.tsx
+++ components/Card/AreaCard/AreaCard.tsx
@@ -1,42 +1,45 @@
-import Image, { StaticImageData } from 'next/image';
-import React from 'react'
+import Image, { StaticImageData } from "next/image";
 
 interface AreaCardProps {
   src: StaticImageData;
   alt: string;
   areaTitle: string;
   areaDescription: string;
+  tag1?: string;
+  tag2?: string;
+  tag3?: string;
 }
 
-const AreaCard = ({src, alt, areaTitle, areaDescription}: AreaCardProps) => {
+const AreaCard = ({
+  src,
+  alt,
+  areaTitle,
+  areaDescription,
+  tag1,
+  tag2,
+  tag3,
+}: AreaCardProps) => {
   return (
-    <div className='mb-16'>
-      <div className='mb-4'>
+    <div className="mb-16">
+      <div className="mb-4">
         <Image src={src} alt={alt} />
       </div>
       <div>
-        <div className='flex items-center gap-x-4'>
+        <div className="flex items-center gap-x-4">
           <div>
-            <h3 className=''>
-              {areaTitle}
-            </h3>
+            <h3 className="">{areaTitle}</h3>
           </div>
-          <div className='flex gap-x-2'>
-            <span className='areaTags'>
-              Montélimar
-            </span>
-            <span className='areaTags'>
-              Puygiron
-            </span>
+          <div className="flex gap-x-2">
+            {tag1 && <span className="areaTags">{tag1}</span>}
+            {tag2 && <span className="areaTags">{tag2}</span>}
+            {tag3 && <span className="areaTags">{tag3}</span>}
           </div>
         </div>
 
-        <p className='text-sm md:text-sm'>
-          {areaDescription}
-        </p>
+        <p className="text-sm md:text-sm">{areaDescription}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AreaCard
+export default AreaCard;
