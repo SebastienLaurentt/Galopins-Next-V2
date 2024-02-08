4b4b39a99bb9be978b0014851910a9c4411211d1
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-08 20:05:48 +0100

    create areaData

---
+++ data/areaData.ts
@@ -0,0 +1,23 @@
+const areaData = [
+  {
+    id: 0,
+    src: "logoDrome",
+    alt: "Lavande de la Drôme",
+    areaTitle: "Drôme",
+    areaDescription: "Le département de la Drôme est un lieu de randonnée idéal pour les Galopins."
+  },
+  {
+    id: 1,
+    src: "logoArdeche",
+    alt: "Valon Pont d'Arc en Ardèche",
+    areaTitle: "Ardèche",
+    areaDescription: "Le département de l'Ardèche offre des paysages spectaculaires et des parcours variés pour les amateurs de randonnée."
+  },
+  {
+    id: 2,
+    src: "logoGard",
+    alt: "Pont du Gard",
+    areaTitle: "Gard",
+    areaDescription: "Avec le Pont du Gard et ses nombreux sentiers, le Gard est un département riche en découvertes pour les randonneurs."
+  }
+];
