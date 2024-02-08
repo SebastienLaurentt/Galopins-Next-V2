ea3529482f8f1a1e835a9367c97d7f7efa1fca02
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-08 01:09:36 +0100

    header for lg display

--- src/features/layout/Header/BurgerMenu.tsx
+++ src/features/layout/Header/BurgerMenu.tsx
@@ -46,7 +46,7 @@ export default function BurgerMenu({ isOpen, setIsOpen }: IBurgerMenu) {
         onClick={() => setIsOpen(true)}
         aria-label="Ouvrir le menu mobile"
       >
-        <Menu color="#fff" className="h-8 md:h-10 w-8 md:w-10 " />
+        <Menu color="#fff" className="h-8 md:h-10 w-8 md:w-10" />
       </button>
 
       <nav
--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -11,11 +11,35 @@ export const Header = () => {
 
   return (
     <header className="absolute text-white w-full cursor-pointer z-10 ">
-      <div className="flex items-center px-6 md:px-10 py-4 justify-between  ">
+      <div className="flex items-center px-6 md:px-10 py-4 justify-between   ">
         <Link href="/">
-          <Image src={logo} alt="Logo Galopins" className="w-16 md:w-20" />
+          <Image src={logo} alt="Logo Galopins" className="w-16 md:w-20 lg:w-24" />
         </Link>
-        <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+
+        <nav className="hidden lg:block lg:text-md">
+          <ul className="flex gap-4">
+            <li>
+              <Link href="/">Accueil</Link>
+            </li>
+            <li>
+              <Link href="/club">Le Club</Link>
+            </li>
+            <li>
+              <Link href="/parcours">Les Parcours</Link>
+            </li>
+            <li>
+              <Link href="/photos">Nos Photos</Link>
+            </li>
+            <li>
+              <Link href="/rejoindre">Nous Rejoindre</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="lg:hidden">
+          <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
+        
       </div>
     </header>
   );
