540266b1e7be6321df19259792fda5fb3cbaeab0
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-09 02:29:37 +0100

    create first section in Parcours Page

--- app/parcours/page.tsx
+++ app/parcours/page.tsx
@@ -1,4 +1,7 @@
+import ParcoursCard from "@/components/Card/ParcoursCard/ParcoursCard";
 import PageTitle from "@/components/PageTitle/PageTitle";
+import Section from "@/components/Section/Section";
+import SectionHeader from "@/components/Section/SectionHeader/SectionHeader";
 
 function Parcours() {
   return (
@@ -7,6 +10,51 @@ function Parcours() {
         title="Les Parcours"
         titleDescription="Pour en savoir plus sur les types de randonnées que nous proposons mais aussi sur le programme de la saison à venir !"
       />
+      <Section marginBottom={true} marginTop={false} classname="">
+        <SectionHeader
+          title="Plusieurs randonnées chaque "
+          titleHighlight="semaine"
+        />
+        <div>
+          <ul className="xl:flex xl:justify-center xl:gap-x-16">
+            <li>
+              <ParcoursCard
+                parcoursTitle="Lundi | Après-Midi"
+                area="Drôme ou Ardèche"
+                frequency="2 types de randonnées"
+                case1={
+                  <>
+                    <strong>&#x2022; Cool :</strong> 5 - 9 km
+                  </>
+                }
+                case2={
+                  <>
+                    <strong>&#x2022; Classique :</strong> 10 - 14 km
+                  </>
+                }
+              />
+            </li>
+            <li>
+              <ParcoursCard
+                parcoursTitle="Vendredi | Journée entière"
+                area="Vaucluse, Gard ou Lozère"
+                frequency="En alternance"
+                case1={
+                  <>
+                    <strong>&#x2022; Semaine A :</strong> Jusqu&apos;à 15 km
+                  </>
+                }
+                case2={
+                  <>
+                    <strong>&#x2022; Semaine B : </strong> De 15 à 20 km
+                  </>
+                }
+              />
+            </li>
+          </ul>
+        </div>
+      </Section>
+
     </main>
   );
 }
