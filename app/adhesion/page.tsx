36dfcdcbdf1ff4a214711a9dde77f907f0bf3c4e
Sebastien Laurent <slaurent.26@gmail.com>
2024-02-09 14:26:04 +0100

    beginning adhesion page

--- app/adhesion/page.tsx
+++ app/adhesion/page.tsx
@@ -1,8 +1,30 @@
+import PageTitle from '@/components/PageTitle/PageTitle'
+import Section from '@/components/Section/Section'
+import SectionHeader from '@/components/Section/SectionHeader/SectionHeader'
 import React from 'react'
 
 function Rejoindre() {
   return (
-    <div>Rejoindre</div>
+    <main className="mb-20 md:mb-24 lg:mb-32">
+      <PageTitle
+        title="Adhésion"
+        titleDescription="Nos conseils et recommandations si vous souhaitez nous rejoindre !"
+      />
+      <Section marginBottom={true} marginTop={false} classname="">
+        <SectionHeader
+          title="Comment se préparer à "
+          titleHighlight="randonner"
+        />
+      </Section>
+      
+      <Section marginBottom={true} marginTop={false} classname="">
+        <SectionHeader
+          title=" Des questions ?"
+          titleHighlight="Contactez-nous ! "
+      />
+
+      </Section>
+    </main>
   )
 }
 
