50cb81d952071c472b20147e3c658172ff47b8ef
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-09 02:03:07 +0100

    import page title in Parcours Page

--- app/parcours/page.tsx
+++ app/parcours/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import PageTitle from "@/components/PageTitle/PageTitle";
 
 function Parcours() {
   return (
-    <div>Parcours</div>
-  )
+    <main>
+      <PageTitle
+        title="Parcours"
+        titleDescription="Les Galopins sortent en groupe pour randonner principalement dans les départements de la Drôme, de l'Ardèche et du Gard. Cependant, le Vaucluse et la Lozère peuvent également être choisis pour des randonnées plus lointaines."
+      />
+    </main>
+  );
 }
 
-export default Parcours
+export default Parcours;
--- components/PageTitle/PageTitle.tsx
+++ components/PageTitle/PageTitle.tsx
@@ -10,14 +10,14 @@ const PageTitle: React.FC<PageTitleProps> = ({
   titleDescription,
 }) => {
   return (
-    <div className="mb-12 md:mb-16">
+    <section className="mb-12 md:mb-16">
       <h1>
         {title}
       </h1>
       <p className="">
         {titleDescription}
       </p>
-    </div>
+    </section>
   );
 };
 
