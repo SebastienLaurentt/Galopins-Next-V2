0fafe4adf107d96fc153c2c0f8253409d79cc0b8
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-12 17:38:55 +0100

    custom not-found page with lottie animation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,24 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-
+import LinkButton from "@/components/Button/LinkButton/LinkButton";
+import ImgAnimation from "@/components/ImgAnimation/Img.Animation";
+import Section from "@/components/Section/Section";
 
 export default function NotFound() {
-
+  const animation =
+    "https://lottie.host/5b46926b-3fb0-4a93-b3a6-a96ffb7c537b/ZaidZSakBt.json";
   return (
-    <div className="text-center lg:py-4">
-      <h1 className="text-3xl">
-        Page non existante
-      </h1>
-      <Button>
-        <Link href="/"> Retourner à l&apos;accueil </Link>
-      </Button>
-    </div>
+    <main className="mb-20">
+      <Section marginBottom={false} marginTop={false} classname="text-center mt-12 lg:mt-20 xl:mt-8 max-w-[360px] md:max-w-[500px] lg:max-w-[600px] xl:max-w-[1280px] mx-auto">
+        <div className="mb-8 lg:mb-16 xl:mb-8">
+          <h1 className="mb-4">La page n&apos;existe pas !</h1>
+          <p>Il semblerait que vous vous soyez trompez de chemin !</p>
+        </div>
+        <ImgAnimation animation={animation} />
+        <LinkButton
+          seeMoreButton={true}
+          href="/parcours"
+          linkName="Retourner à l'accueil"
+        />
+      </Section>
+    </main>
   );
 }
---
+++ components/ImgAnimation/Img.Animation.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Player } from "@lottiefiles/react-lottie-player";
+
+export default function ImgAnimation ({ animation }: { animation: string }) {
+  return (
+    <Player
+      autoplay
+      loop
+      src={animation}
+      className="h-[250px] md:h-[350px]  w-[250px] md:w-[350px]"
+    ></Player>
+  );
+}
