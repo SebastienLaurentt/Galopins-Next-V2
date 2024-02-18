9839a236f83f089509f85fac9978c9ee7ef9c0bb
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-18 22:40:15 +0100

    update Home hero bg

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 import logoArdeche from "../public/images/Ardeche.jpg";
 import logoDrome from "../public/images/Drome.jpg";
 import logoGard from "../public/images/Gard.jpg";
-import img from "../public/images/Home2.webp";
+import img from "../public/images/Home.jpg";
 
 
 
