288a1fad06209fbb6a661f3051f08a7160575e09
Sebastien Laurent <slaurent.26@gmail.com>
2024-02-24 23:30:47 +0100

    new method to contact form with resend

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,73 +1,79 @@
-'use client';
+"use client";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
+import { sendEmail } from "../../src/actions";
 
-import { FC } from 'react';
-import { useForm } from 'react-hook-form';
-import { sendEmail } from '../../app/utils/send-email';
-
-export type FormData = {
-  name: string;
-  email: string;
-  message: string;
-};
-
-const Contact: FC = () => {
-  const { register, handleSubmit } = useForm<FormData>();
-
-  function onSubmit(data: FormData) {
-    sendEmail(data);
-  }
+export default function Form() {
+  const [sendEmailState, sendEmailAction] = useFormState(sendEmail, {
+    error: null,
+    success: false,
+  });
+  useEffect(() => {
+    if (sendEmailState.success) {
+      alert("Email sent!");
+    }
+    if (sendEmailState.error) {
+      alert("Error sending email!");
+    }
+  }, [sendEmailState]);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md mx-auto">
-      <div className='mb-2'>
+    <form action={sendEmailAction}>
+      <div className="mb-5">
         <label
-          htmlFor='name'
-          className='mb-3 block text-base font-medium text-black'
+          htmlFor="name"
+          className="mb-3 block text-base font-medium text-black"
         >
           Full Name
         </label>
         <input
-          type='text'
-          placeholder='Full Name'
-          className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md'
-          {...register('name', { required: true })}
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Full Name"
+          className="w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
         />
       </div>
-      <div className='mb-5'>
+      <div className="mb-5">
         <label
-          htmlFor='email'
-          className='mb-3 block text-base font-medium text-black'
+          htmlFor="email"
+          className="mb-3 block text-base font-medium text-black"
         >
           Email Address
         </label>
         <input
-          type='email'
-          placeholder='example@domain.com'
-          className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md'
-          {...register('email', { required: true })}
+          type="email"
+          id="email"
+          name="email"
+          placeholder="example@domain.com"
+          className="w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
         />
       </div>
-      <div className='mb-5'>
+      <div className="mb-5">
         <label
-          htmlFor='message'
-          className='mb-3 block text-base font-medium text-black'
+          htmlFor="message"
+          className="mb-3 block text-base font-medium text-black"
         >
           Message
         </label>
         <textarea
           rows={4}
-          placeholder='Type your message'
-          className='w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md'
-          {...register('message', { required: true })}
+          id="message"
+          name="message"
+          placeholder="Type your message"
+          className="w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md"
         ></textarea>
       </div>
       <div>
-        <button className='hover:shadow-form rounded-md bg-purple-500 py-3 px-8 text-base font-semibold text-white outline-none'>
+        <button
+          className="hover:shadow-form rounded-md bg-purple-500 py-3 px-8 text-base font-semibold text-white outline-none"
+          type="submit"
+        >
           Submit
         </button>
       </div>
     </form>
   );
-};
+}
 
-export default Contact;
+
