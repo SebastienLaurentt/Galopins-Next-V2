4e7c09b9eb99e00c562fa032287dca1520e9c934
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-29 15:53:16 +0100

    update logo in header and update header css

--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import navData from "../../../../data/navData";
-import logo from "../../../../public/images/logoGalopins.png";
+import logo from "../../../../public/images/logoGalopins.svg";
 import BurgerMenu from "./BurgerMenu";
 
 export const Header = () => {
@@ -13,18 +13,18 @@ export const Header = () => {
   const pathname = usePathname();
 
   const headerBgColor = pathname === "/" ? "" : "bg-white";
-  const headerPosition = pathname === "/" ? "absolute bg-white" : "";
+  const headerPosition = pathname === "/" ? "absolute " : "";
 
   return (
     <header
-      className={`${headerPosition} text-dark font-bold w-full z-10 ${headerBgColor}`}
+      className={`${headerPosition} text-white font-bold w-full z-10 ${headerBgColor}`}
     >
       <div className="flex items-center px-6 md:px-10 xl:px-16 xl:max-w-[1280px] xl:mx-auto py-4 justify-between ">
         <Link href="/">
           <Image
             src={logo}
             alt="Logo Galopins"
-            className="w-20 md:w-24 lg:w-28"
+            className="w-20 md:w-24 lg:w-28 text-white"
           />
         </Link>
 
@@ -36,7 +36,7 @@ export const Header = () => {
                   href={link.href}
                   className={
                     pathname === link.href
-                      ? "border-b-2 border-black font-bold"
+                      ? "border-b-2 border-white "
                       : ""
                   }
                 >
