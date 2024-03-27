380e3f903d01ab13a48a6487bcd1d6d9f5712dec
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-27 16:50:14 +0100

    create actualites card

---
+++ components/Card/ActualitesCard/ActualitesCard.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+interface ActualitesCardProps {}
+
+const ActualitesCard = ({
+
+}: ActualitesCardProps) => {
+  return (
+    <div className="">
+      Yooo
+    </div>
+  );
+};
+
+export default ActualitesCard;
