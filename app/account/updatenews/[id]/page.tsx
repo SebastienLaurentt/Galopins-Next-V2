672feb99c48e2a2063a6d48c24344537a46835be
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-27 19:38:07 +0100

    use prettier

--- app/account/updatenews/[id]/page.tsx
+++ app/account/updatenews/[id]/page.tsx
@@ -1,21 +1,21 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import Input from '../../../../components/AccountComponent/Form/Input';
-import AccountHeader from '../../../../components/AccountComponent/AccountHeader';
-import Textarea from '../../../../components/AccountComponent/Form/Textarea';
-import ValidationButton from '../../../../components/AccountComponent/Button/ValidationButton';
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import AccountHeader from "../../../../components/AccountComponent/AccountHeader";
+import ValidationButton from "../../../../components/AccountComponent/Button/ValidationButton";
+import Input from "../../../../components/AccountComponent/Form/Input";
+import Textarea from "../../../../components/AccountComponent/Form/Textarea";
 
-import { useParams, useRouter } from 'next/navigation';
-import { useAuth } from '@/components/AccountComponent/Auth/Auth';
+import { useAuth } from "@/components/AccountComponent/Auth/Auth";
+import { useParams, useRouter } from "next/navigation";
 
 const AccountNewsUpdate = () => {
   const { id } = useParams(); // Obtenir l'ID de la nouvelle depuis l'URL
-  const [date, setDate] = useState('');
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [date, setDate] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const [loadingData, setLoadingData] = useState(true); // Ajouter un état de chargement pour les données
   const [loadingSubmit, setLoadingSubmit] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
@@ -24,19 +24,18 @@ const AccountNewsUpdate = () => {
   const router = useRouter();
 
   useEffect(() => {
-
     if (!isLogged) {
       // Redirigez l'utilisateur vers la page de connexion s'il n'est pas connecté
-      router.push('/login');
+      router.push("/login");
       return;
     }
 
     const fetchData = async () => {
       try {
-        const token = Cookies.get('token');
+        const token = Cookies.get("token");
 
         if (!token) {
-          console.error('Le jeton n\'est pas disponible.');
+          console.error("Le jeton n'est pas disponible.");
           return;
         }
 
@@ -49,15 +48,18 @@ const AccountNewsUpdate = () => {
           }
         );
 
-        const dataNouvelle = response.data.data; 
+        const dataNouvelle = response.data.data;
 
-        setDate(dataNouvelle.date || '');
-        setTitle(dataNouvelle.title || '');
-        setDescription(dataNouvelle.description || '');
+        setDate(dataNouvelle.date || "");
+        setTitle(dataNouvelle.title || "");
+        setDescription(dataNouvelle.description || "");
 
         setLoadingData(false);
       } catch (error) {
-        console.error('Erreur lors de la récupération des données de la nouvelle :', error);
+        console.error(
+          "Erreur lors de la récupération des données de la nouvelle :",
+          error
+        );
       }
     };
 
@@ -69,10 +71,10 @@ const AccountNewsUpdate = () => {
 
     try {
       setLoadingSubmit(true);
-      const token = Cookies.get('token');
+      const token = Cookies.get("token");
 
       if (!token) {
-        console.error('Le jeton n\'est pas disponible.');
+        console.error("Le jeton n'est pas disponible.");
         return;
       }
 
@@ -85,7 +87,7 @@ const AccountNewsUpdate = () => {
         },
         {
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
         }
@@ -99,10 +101,10 @@ const AccountNewsUpdate = () => {
       // Masquer le message de succès après 2 secondes et rediriger
       setTimeout(() => {
         setShowSuccessMessage(false);
-        router.push('/account');
+        router.push("/account");
       }, 2000);
     } catch (error) {
-      console.error('Erreur lors de la mise à jour de la nouvelle :', error);
+      console.error("Erreur lors de la mise à jour de la nouvelle :", error);
     } finally {
       setLoadingSubmit(false);
     }
@@ -112,19 +114,22 @@ const AccountNewsUpdate = () => {
     <div className="min-h-screen bg-stone-300 p-4 ">
       <AccountHeader />
       <main>
-        <h3 className="text-black text-center m-4">
+        <h3 className="m-4 text-center text-black">
           Formulaire de mise à jour de l&apos;information
         </h3>
 
         {loadingData ? (
           <div className="flex flex-col items-center gap-y-4">
-            <p className="text-black"> Chargement des données de l&apos;information</p>
+            <p className="text-black">
+              {" "}
+              Chargement des données de l&apos;information
+            </p>
           </div>
         ) : (
-          <div className="flex flex-col justify-center items-center justify-center bg-stone-300 p-4 mt-8">
+          <div className="mt-8 flex flex-col items-center justify-center bg-stone-300 p-4">
             <form
               onSubmit={handleSubmit}
-              className="flex flex-col gap-y-4 text-center bg-slate-900 p-8 rounded-md"
+              className="flex flex-col gap-y-4 rounded-md bg-slate-900 p-8 text-center"
             >
               <Input
                 inputName="Date"
@@ -147,22 +152,22 @@ const AccountNewsUpdate = () => {
               />
 
               {/* Conditionally render the Loader based on the loading state */}
-              {loadingSubmit ? (
-                <div className="flex justify-center">
-                  <span>Chargement...</span>
-                </div>
-              ) : null /* Ne pas rendre le bouton si loadingSubmit est vrai */}
+              {
+                loadingSubmit ? (
+                  <div className="flex justify-center">
+                    <span>Chargement...</span>
+                  </div>
+                ) : null /* Ne pas rendre le bouton si loadingSubmit est vrai */
+              }
               {/* Conditionally render the success message */}
               {showSuccessMessage && (
-                <span className="text-green-600 mt-2 text-md">
+                <span className="mt-2 text-md text-green-600">
                   Info mise à jour ! Redirection...
                 </span>
               )}
               {/* Render the button outside the form to prevent it from re-rendering */}
               {!loadingSubmit && !showSuccessMessage && (
-                <ValidationButton
-                  buttonName="Mettre à jour l'information"
-                />
+                <ValidationButton buttonName="Mettre à jour l'information" />
               )}
             </form>
           </div>
