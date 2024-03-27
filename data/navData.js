a0a09c2f7dca0a7eecb5de20bd9d2234b91c505c
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-27 16:40:31 +0100

    add actualites in navData

--- data/navData.js
+++ data/navData.js
@@ -1,28 +1,33 @@
 const navData = [
   {
-      href: '/',
-      name: 'Accueil',
-      burgerMenuName:'Accueil',
+    href: "/",
+    name: "Accueil",
+    burgerMenuName: "Accueil",
   },
   {
-      href: '/club',
-      name: 'Club',
-      burgerMenuName:'Le Club',
+    href: "/club",
+    name: "Club",
+    burgerMenuName: "Le Club",
   },
   {
-      href: '/parcours',
-      name: 'Parcours',
-      burgerMenuName:'Les Parcours',
+    href: "/parcours",
+    name: "Parcours",
+    burgerMenuName: "Les Parcours",
   },
   {
-      href: '/photos',
-      name: 'Photos',
-      burgerMenuName:'Nos Photos',
+    href: "/photos",
+    name: "Photos",
+    burgerMenuName: "Nos Photos",
   },
   {
-      href: '/adhesion',
-      name: 'Adhésion',
-      burgerMenuName:'Adhésion',
+    href: "/actualites",
+    name: "Actualités",
+    burgerMenuName: "Actualités",
+  },
+  {
+    href: "/adhesion",
+    name: "Adhésion",
+    burgerMenuName: "Adhésion",
   },
 ];
 
