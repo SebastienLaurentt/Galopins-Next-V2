308714a73c92d98aa722901fcfd690eb9510c8c6
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-27 17:36:24 +0100

    custom css on ActualitesCard

--- components/Card/ActualitesCard/ActualitesCard.tsx
+++ components/Card/ActualitesCard/ActualitesCard.tsx
@@ -4,8 +4,14 @@ interface ActualitesCardProps {
   description: string;
 }
 
-const ActualitesCard = ({title, date, description}: ActualitesCardProps) => {
-  return <div className="">Yooo</div>;
+const ActualitesCard = ({ title, date, description }: ActualitesCardProps) => {
+  return (
+    <div className="rounded-lg border p-4 text-left shadow-lg">
+      <h4 className="mb-2 text-md font-bold xl:text-xl">{title}</h4>
+      <p className="mb-2 text-pretty">{description}</p>
+      <p className="italic">le {date}</p>
+    </div>
+  );
 };
 
 export default ActualitesCard;
