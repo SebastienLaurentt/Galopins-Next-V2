8fb99f4eb35cfd93b6a761d89a8319b5be53ceaf
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-22 10:49:56 +0200

    update file upload design

--- app/(auth)/account/addrando/page.tsx
+++ app/(auth)/account/addrando/page.tsx
@@ -149,11 +149,11 @@ const AccountRandoAdd = () => {
   return (
     <div className="p-4">
       <main className="text-white">
-        <h3 className="m-4 mx-auto w-[500px] text-center text-black">
+        <h3 className="m-1 mx-auto w-[500px] text-center text-black">
           Formulaire d&apos;ajout d&apos;une nouvelle randonnée
         </h3>
 
-        <div className="mt-8 flex flex-col items-center justify-center  p-4">
+        <div className="mt-3 flex flex-col items-center justify-center  p-4">
           <form
             onSubmit={handleSubmit}
             className="flex w-[300px] flex-col gap-y-4 rounded-md bg-slate-900 p-8 text-center md:w-[400px] lg:w-[500px]"
@@ -202,13 +202,13 @@ const AccountRandoAdd = () => {
                 isNumber
               />
             </div>
-            <div className="flex flex-col gap-y-1 text-md">
+            <div className="flex flex-col gap-y-1 text-sm">
               <label>
-                Images
-                <div className="flex cursor-pointer flex-col items-center   justify-center">
+                <span className="font-bold">Images</span>
+                <div className="flex cursor-pointer flex-col items-center justify-center">
                   <AiOutlinePicture size={64} />
                   <p className="mb-0">
-                    <span className="text-md">
+                    <span className="text-sm">
                       Cliquer pour sélectionner les images
                     </span>
                   </p>
