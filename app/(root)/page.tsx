a18e42273ff093e8f9fa9e85cf3d37bc4f0dfa20
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-25 15:44:13 +0200

    update parallax from xl display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -55,11 +55,11 @@ export default function Home() {
 
       <section className="parallax-container mx-auto mt-8 hidden xl:flex xl:px-4">
         <div
-          className="parallax relative mx-auto w-full overflow-hidden rounded-3xl lg:h-[600px] 2xl:h-[800px]"
+          className="parallax relative mx-auto w-full overflow-hidden rounded-md xl:h-[800px]"
           style={{ backgroundImage: `url(${img.src})` }}
         >
           <div className="parallax-overlay absolute bg-black opacity-50"></div>
-          <div className="relative mx-auto text-center text-white lg:mt-40 lg:max-w-[680px] lg:space-y-4 2xl:mt-56 ">
+          <div className="relative mx-auto text-center text-white lg:space-y-4 xl:mt-72 xl:max-w-[800px] ">
             <h1>Les Galopins de Montélimar</h1>
             <p className="text-md leading-6">
               Bienvenue sur le site de notre club.
