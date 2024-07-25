07b497a26201d3b13de0530d18950a799a7974b4
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-25 18:18:25 +0200

    fix header for md display

--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -37,7 +37,7 @@ export const Header = () => {
               >
                 <Link
                   href={link.href}
-                  className={`rounded-full px-6 py-2 font-semibold hover:bg-slate-950 hover:text-white ${
+                  className={`rounded-full px-3 py-1.5 font-semibold hover:bg-slate-950 hover:text-white lg:px-6 lg:py-2 ${
                     pathname === link.href ? "bg-slate-950 text-white" : ""
                   }`}
                 >
