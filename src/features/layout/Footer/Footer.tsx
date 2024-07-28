1a42c4c352137c22edee24cc11e7ff8fc795ae7a
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-28 15:47:32 +0200

    update css

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -11,13 +11,13 @@ export const Footer = () => {
 
   return (
     <footer
-      className={` ${footerVisibility}  bg-accent pb-6 pt-8 text-white  `}
+      className={` ${footerVisibility}  bg-accent pb-6 pt-8 xl:pb-10 xl:pt-12 text-white  `}
     >
       <div className="flex flex-col gap-y-5 px-6 md:px-10 xl:mx-auto xl:max-w-screen-xl  xl:px-16 ">
         <div className="flex flex-col md:flex-row md:items-center md:justify-between ">
           <div className="mb-5 flex flex-col items-center md:mb-0 md:w-1/2 md:items-start">
             <span
-              className={`${anton} w-[280px] text-center text-xl font-semibold leading-9 md:mb-1 md:w-fit md:text-left md:text-2xl xl:text-3xl xl:leading-10`}
+              className={`${anton} w-[280px] text-center text-xl font-semibold leading-9 md:mb-1 md:w-fit md:text-left md:text-2xl xl:mb-2 xl:text-3xl xl:leading-10`}
             >
               LES GALOPINS DE MONTELIMAR
             </span>
@@ -59,8 +59,8 @@ export const Footer = () => {
           </ul>
         </div>
         <div className="flex items-center justify-between text-[14px]  md:text-sm  ">
-          <span className="">@2024 - Les Galopins</span>
-          <span className="">
+          <span>@2024 - Les Galopins</span>
+          <span>
             <Link href="/mentions"> Mentions légales </Link>
           </span>
         </div>
