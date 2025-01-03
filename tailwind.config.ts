2d0ad4b51bb07ea6044590e11bd12843e2b06dd4
SebastienLaurentt <slaurent.26@gmail.com>
2025-01-03 13:27:24 +0100

    update style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,14 +86,14 @@ const config = {
           to: { height: "0" },
         },
         marquee: {
-          "0%": { transform: "translateX(-100%)" },
-          "100%": { transform: "translateX(100%)" },
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        marquee: "marquee 15s linear infinite",
+        marquee: "marquee 10s linear infinite",
       },
     },
   },
